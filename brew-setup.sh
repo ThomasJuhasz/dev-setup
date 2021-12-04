9539b3434f999e9ec3e060912f7b2b85109f3413
Thomas Juhász <juhasz.thomas.jozsef@gmail.com>
2021-12-04 12:58:59 +0100

    Create brew-setup.sh

---
+++ brew-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install google-chrome
+
+# iterm2 with theme
+brew install iterm2
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+brew install visual-studio-code
