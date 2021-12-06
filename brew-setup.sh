4d0bed6f8607a654d4b4f2c04c596039a25534a4
Thomas Juhász <juhasz.thomas.jozsef@gmail.com>
2021-12-06 09:57:17 +0100

    Update brew-setup.sh

--- brew-setup.sh
+++ brew-setup.sh
@@ -3,9 +3,10 @@
 #install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install --cask 1password
 brew install google-chrome
 brew install git
-brew install --cask gitkraken
+brew install visual-studio-code
 
 # iterm2 with theme
 brew install iterm2
@@ -13,4 +14,14 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
-brew install visual-studio-code
+brew install --cask monitorcontrol
+
+brew install --cask workflowy
+brew install --cask gitkraken
+brew install --cask snagit
+brew install --cask foxitreader
+brew install --cask miro
+brew install docker
+brew install --cask postman
+
+
